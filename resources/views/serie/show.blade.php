4d000c2534ea04184f9c97754a4ca8c01934b502
firstphon <firt3@hotmail.com>
2020-02-08 15:06:45 +0700

    เกือบได้วิดีโอแล้ว

---
+++ resources/views/serie/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('partials.navbar')
+
+<div class="container mt-5">
+    <h1 class="py-5">{{$serie->title}}</h1>
+    <a class="btn btn-primary" href="{{url('/series/'.$serie->id.'/episodes/create')}}">เพิ่มตอน</a>
+    <br/><br/>
+    <ul>
+        @foreach($serie->episodes as $episode)
+        <li>
+            <a href="{{url('')}}">{{ $episode->title }}</a>
+        </li>
+        @endforeach
+    </ul>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,64 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/test-video', function () {
-    return view('partials.url-video-player');
+    return view('partials.vimeo-video-player');
+});
+
+//แสดงรายการ ซี่รี่ส์/ตอน index
+Route::get('/series', function(){
+    $series=\App\Serie::all();
+    return view('serie.index')->with(['series'=>$series]);
+
+})->name('series');
+
+//แสดงฟอร์มสร้าง ซี่รี่ส์/ตอน
+Route::get('/series/create',function(){
+    return view('serie.create');
+})->name('create');
+Route::get('/series/{serieid}/episodes/create',function($serieid){
+    return view('episode.create')->with(['serieid' => $serieid]);
+});
+
+//รับข้อมูลจากฟอร์มสร้าง ซีรีย์/ตอน แล้วบันทึกลงตาราง
+Route::POST('/series',function(){
+    $data = \Request::all();
+
+    //return $data;
+
+    //{"_token"=>"f2eNfaXvGHkFYPJ9iSrsa0Bq9R1Wn511L5VqkquF","title"=>"test"}
+
+    $episode = \App\Serie::create($data);
+
+    return redirect('series');
+});
+
+Route::POST('/serie/{id}/episodes',function($id){
+    $data = \Request::all();
+
+    $episode = \App\Episode::create($data);
+
+    $episode->serie_id = $id;
+
+    $episode->save();
+
+    return redirect('/series/'. $id);
+});
+
+//แสดงตอนที่อยู่ใน ซี่รี่ย์ 
+// Route::get('/series/{id}',function($id){ แบบแรก
+//     $serie=\App\serie::find($id);
+//     return view('serie.show')->with([
+//         'serie' => $serie
+//     ]);
+//     return $id;
+
+// });
+
+Route::get('/series/{serie}',function(\App\Serie $serie){
+    return view('serie.show')->with([
+        'serie' => $serie
+    ]);
+    return $id;
+
 });
 
